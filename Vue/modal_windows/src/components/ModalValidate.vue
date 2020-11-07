d2edfb819c1834414fc1a3fe4f4d56a9ad3ca5ff
CommanderXA <53992051+CommanderXA@users.noreply.github.com>
2020-11-07 10:00:59 +0600

    Day 4 - With Base Homework

--- Vue/modal_windows/src/components/ModalValidate.vue
+++ Vue/modal_windows/src/components/ModalValidate.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <modals title="The Validate Modal" @close="$emit('close')">
+    <modals title="The Validate Modal" @close=onclose>
 
             <div slot="body">
               <form @submit.prevent="onSubmit">
@@ -21,6 +21,25 @@
                   :class="{ error: $v.email.$error }"
                   @change="$v.email.$touch()">
                 </div>
+
+                <div class="form-item" :class="{ errorInput: $v.password.$error }">
+                  <label>Password</label>
+                  <p class="errorText" v-if="!$v.password.required">This Filed is required</p>
+                  <p class="errorText" v-if="!$v.password.minLength">Password contain at least 6 symbols</p>
+                  <input v-model="password" 
+                  :class="{ error: $v.password.$error }"
+                  @change="$v.password.$touch()">
+                </div>
+
+                <div class="form-item" :class="{ errorInput: $v.repeatPassword.$error }">
+                  <label>Repeat Password</label>
+                  <p class="errorText" v-if="!$v.repeatPassword.required">This Filed is required</p>
+                  <p class="errorText" v-if="!$v.repeatPassword.sameAsPassword">Passwords must match</p>
+                  <input v-model="repeatPassword" 
+                  :class="{ error: $v.repeatPassword.$error }"
+                  @change="$v.repeatPassword.$touch()">
+                </div>
+
                 <button class="btn btnPrimary">Submit</button>
               </form>
             </div>
@@ -30,7 +49,7 @@
 </template>
 
 <script>
-import { required, minLength, email } from 'vuelidate/lib/validators'
+import { required, minLength, email, sameAs } from 'vuelidate/lib/validators'
 import modals from "@/components/UI/Modal.vue"
 export default {
   components: {
@@ -39,7 +58,9 @@ export default {
   data() {
     return {
       name: '',
-      email: ''
+      email: '',
+      password: '',
+      repeatPassword: ''
     }
   },
   validations: {
@@ -50,6 +71,14 @@ export default {
     email: {
       required,
       email
+    },
+    password: {
+      required,
+      minLength: minLength(6)
+    },
+    repeatPassword: {
+      required,
+      sameAsPassword: sameAs('password')
     }
   },
   methods: {
@@ -58,15 +87,27 @@ export default {
       if (!this.$v.$invalid) {
         const user = {
           name: this.name,
-          email: this.email
+          email: this.email,
+          password: this.password,
+          sameAsPassword: this.sameAsPassword,
         }
         console.log(user);
         // DONE
         this.name = ''
         this.email = ''
+        this.password = ''
+        this.repeatPassword = ''
         this.$v.$reset()
         this.$emit('close')
       }
+    },
+    onclose() {
+      this.name = ''
+      this.email = ''
+      this.password = ''
+      this.repeatPassword = ''
+      this.$v.$reset()
+      this.$emit('close')
     }
   }
 }
@@ -84,7 +125,7 @@ export default {
   display: block;
   }
 }
-input .error {
+.form-item .error {
   border-color: #de4040;
 }
 </style>
